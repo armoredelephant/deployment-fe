d1cad40e0cf45560cf510b66e530aef0c5881785
keith alleman <keith.alleman@icloud.com>
2019-12-22 19:25:08 -0700

    adding notes for deployment component

---
+++ src/components/create-deployment/CreateDeploymentPage.tsx
@@ -0,0 +1,13 @@
+// in the onSubmit we will loop through the data and for each input, we will post.
+
+/**
+ * top form that asks:
+ * How many users are you deploying equipment to? Dropdown with #?
+ * Will these be remote setups? No defaulted, with Yes radio button
+ * Primary PC for the majority? Laptop/Desktop/Igel
+ */
+
+/**
+ * bottom form is populated based on responses:
+ *
+ */
